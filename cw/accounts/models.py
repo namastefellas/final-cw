721d57391b91c2f6ad48865b671bdde0615c9511
German <coolmatoff@gmail.com>
2021-08-21 18:05:17 +0600

    created views for login, logout, registration for users

--- cw/accounts/models.py
+++ cw/accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='User', blank=True, null=True)
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Avatar')
+    
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        db_table = 'profiles'
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
--- cw/accounts/views.py
+++ cw/accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import MyUserCreationForm
 
-# Create your views here.
+def login_view(request, *args, **kwargs):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('advert:advert_list')
+        context['has_error'] = True
+    return render(request, 'login.html', context=context)
+
+
+@login_required
+def logout_view(request, *args, **kwargs):
+    logout(request)
+    return redirect('advert:advert_list')
+
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = MyUserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            
+            login(request, user)
+            return redirect('advert:advert_list')
+    else:
+        form = MyUserCreationForm()
+    return render(request, 'user_create.html', context={'form': form})
