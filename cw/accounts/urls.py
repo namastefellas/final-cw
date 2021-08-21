da9d279218345cd07406a2876d6da741e824b8eb
German <coolmatoff@gmail.com>
2021-08-21 18:10:32 +0600

    added urls.py file in accounts and some minor fixes in advert templates

---
+++ cw/accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from accounts.views import login_view, logout_view, register_view
+from accounts.profile import UserDetailView
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+    path('user/<int:pk>/profile/', UserDetailView.as_view(), name='user_profile'),
+]
