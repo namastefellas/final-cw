ec264c8b8de3d558b26388f8f7811c2b04e3584b
German <coolmatoff@gmail.com>
2021-08-21 16:13:15 +0600

    added settings for admin page

--- cw/advert/admin.py
+++ cw/advert/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from advert.models import Advert
 
-# Register your models here.
+list_display = [
+    'id',
+    'title',
+    'created_at'
+]
+fields = [
+    'title',
+    'description',
+    'picture',
+    'created_at',
+    'publicated_at',
+    'moderated_at'
+]
+
+admin.site.register(Advert)
